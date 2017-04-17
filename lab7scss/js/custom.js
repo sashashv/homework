bf0352ce0c784481aab9969821cd157b5e5f8006
Sasha Shevchenko <sashash29@mail.ru>
2017-04-17 17:40:14 +0300

    lab7 scss

---
+++ lab7scss/js/custom.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:1,
+                nav:true
+            },
+            992:{
+                items:1,
+                nav:true,
+                loop:true
+            }
+        }
+    });
+});
