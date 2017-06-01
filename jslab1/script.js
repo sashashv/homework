f74e6716a0b782fbfe8abaefadf90fda770082f7
Sasha Shevchenko <sashash29@mail.ru>
2017-06-01 23:42:23 +0300

    script for calc with changes

--- jslab1/script.js
+++ jslab1/script.js
@@ -3,12 +3,16 @@ var a = prompt('Введите первое число', '1'),
     action = prompt('Введите + для сложения, - для вычитания, * для умножения, / для деления'),
     b = prompt('Введите второе число', '1');
 
-
 function isNumeric(x) {
     return !isNaN(parseFloat(x)) && isFinite(x);
 }
 
+function actionTest(action) {
+    return (action == '+' && action == '-' && action == '/' && action == '*');
+}
+
 function calc() {
+
     switch (action) {
         case '+' :
             alert(+a + (+b));
@@ -21,19 +25,23 @@ function calc() {
             break;
         case '/':
             // a/b == Infinity ? alert('На ноль делить нельзя') : alert(a/b);
-            if (a/b === Infinity) {
+            if (a/b == Infinity) {
                 alert('На ноль делить нельзя');
             } else {
                 alert(a/b);
             }
             break;
         default:
-            alert('Введите действие');
+            alert('Введите корректное действие');
     }
 }
-if (isNumeric(a) && isNumeric(b)) {
-    calc();
-} else{
-    alert('Введеные данные не числа');
+
+calc();
+
+while (!(isNumeric(a) && isNumeric(b) && actionTest(action))){
+    alert('Введите числа еще раз');
+    a = prompt('Введите первое число', '1'),
+    action = prompt('Введите + для сложения, - для вычитания, * для умножения, / для деления'),
+    b = prompt('Введите второе число', '1');
 }
 
